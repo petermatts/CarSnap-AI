d5cb23ce1b3687452097b0675efbf523963f83d9
Matthew Peters <mpeters0514@gmail.com>
2025-08-05 23:32:30 -0400

    Shifting from cli to config file focued architecture

--- src/args.py
+++ src/args.py
@@ -1,44 +1,36 @@
 from argparse import ArgumentParser, Namespace
-import yaml
 from enum import StrEnum
 from rich_argparse import ArgumentDefaultsRichHelpFormatter
 
 
 class DatasetArgs(StrEnum):
-    # todo revise construction
     VMMR = "vmmr"
     DMV = "dmv"
 
-    def __str__(self):
+    def __str__(self) -> str:
+        return self.value
+
+    def __repr__(self) -> str:
         return self.value
 
     @staticmethod
-    def from_string(s: str):
-        try:
-            # return DatasetArgs(s.lower()).value
-            return DatasetArgs[s.lower()].value
-        except:
-            raise ValueError(
-                f"Invalid dataset: {s}. Choose from: {[e.value for e in DatasetArgs]}")
+    def argparse(s: str) -> str:
+        def parse():
+            try:
+                return DatasetArgs[DatasetArgs(s).name].value
+            except:
+                raise ValueError(
+                    f"Invalid dataset: {s}. Choose from: {[e.value for e in DatasetArgs]}")
+        return parse
 
 
 def make_args() -> Namespace:
     parser = ArgumentParser(formatter_class=ArgumentDefaultsRichHelpFormatter)
 
-    # dataset args
-    datasets = parser.add_argument_group()
-    datasets.add_argument("--dataset", type=DatasetArgs.from_string,
-                          choices=list(DatasetArgs), help="The dataset to run the expirement on")
-    split = datasets.add_mutually_exclusive_group()
-    split.add_argument("--split", nargs=3, default=[
-                       80, 10, 10], help="train/val/test split in percent. Example: --split 80 10 10. Must sum to 100.")
-    split.add_argument("--nsplit", type=int, nargs=3,
-                       help="train/val/test split in # of elements. Must sum to size of dataset.")
-
-    # training args
-    # training = parser.add_argument_group()
-
-    # saving args
+    # parser.add_argument("--dataset", type=DatasetArgs.argparse(),
+    #                     choices=list(DatasetArgs), help="The dataset to run the expirement on")
+    parser.add_argument("--config", type=str, default="default",
+                        help="name of config yaml file (located in the config folder)")
 
     return parser.parse_args()
 
--- src/expirement.py
+++ src/expirement.py
@@ -1,3 +1,43 @@
 # the main file for running ML expirements using argparse to specify model, hyperparams, datasets, etc.
 
+from pathlib import Path
+import yaml
 from args import make_args
+
+
+class Config:
+    def __init__(self, name: str):
+        self.config_name = name.replace(".yaml", "")
+        self.path = (Path(__file__).parent /
+                     f"../config/{self.config_name}.yaml").resolve()
+        if not self.path.exists():
+            raise FileNotFoundError(
+                f"Could not find config file {self.config_name}.yaml in {self.path.parent}")
+
+        self.dataset = None
+        self.name = None
+        self.lr = None
+        self.optimizer = None
+        self.split = None
+
+        self._load()
+
+        # todo throw error/warning if some config params arent set
+
+    def _load(self):
+        config: dict = None
+        with open(self.path, "r") as f:
+            config = yaml.safe_load(f)
+
+        for k, v in config.items():
+            setattr(self, k, v)
+
+
+def experiment():
+    pass  # todo
+
+
+if __name__ == "__main__":
+    exp_args = make_args()
+
+    config = Config(exp_args.config)
